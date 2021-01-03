1d31e6f633148b3f70c37d6ab1ade59e482775f1
Abdelouahed <medjoudja@sindibadinternational.net>
2021-01-03 21:57:25 +0100

    init heroku

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -2,5 +2,5 @@ abstract class Constants {
   static const String jwtKey = '404hellowordl404';
 
   static const String host =
-      'mongodb+srv://AbdelouahedMedjoudja:abde0698920524@clusterblog.ferri.mongodb.net/<dbname>?retryWrites=true&w=majority'; //'mongodb://localhost:27017/Blog';
+      'mongodb+srv://AbdelouahedMedjoudja:abde0698920524@clusterblog.ferri.mongodb.net/Blog?retryWrites=true&w=majority'; //'mongodb://localhost:27017/Blog';
 }
